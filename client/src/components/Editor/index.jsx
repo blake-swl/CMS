b82e89ab7335b632bf095694883ce44fbfaa30c9
blake-swl <seulwoolee93@gmail.com>
2018-11-08 18:12:57 -0800

    [edit] - firestore db schema

--- client/src/components/Editor/index.jsx
+++ client/src/components/Editor/index.jsx
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import {Editor as Edit, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import Editing from 'react-medium-editor';
+import './editor.scss';
+
+require('medium-editor/dist/css/medium-editor.css');
+require('medium-editor/dist/css/themes/default.css');
 
 export default class Editor extends Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    this.state = {
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(text, medium) {
+    this.setState({text: medium});
+  }
+
   render() {
     return (
       <div id="page-wrap" className="editor">
-        <Edit editorState={this.state.editorState} onChange={this.onChange} />
+        <h1>Edit here</h1>
+        {/* <h3>{this.state.text}</h3> */}
+        <Editing   
+          tag="pre"
+          text={this.state.text}
+          onChange={this.handleChange}
+          options={{toolbar: {buttons: ['bold', 'italic', 'underline', 'h1', 'h2', 'h3']}}}
+          className="texteditor"
+        />
       </div>
     );
   }
--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -13,7 +13,7 @@ import { Icon } from 'react-icons-kit';
 import {user} from 'react-icons-kit/icomoon/user';
 import { home } from 'react-icons-kit/icomoon/home';
 import {list2} from 'react-icons-kit/icomoon/list2';
-// import { QuerySnapshot } from '@firebase/firestore-types';
+import {pencil} from 'react-icons-kit/icomoon/pencil';
 
 
 
@@ -21,24 +21,35 @@ export default class Navigation extends Component {
   constructor() {
     super();
     
-    this.state = {
-      questions: [],
-    }
+    // this.state = {
+    //   questions: [],
+    // }
+    // this.handleDropdown.bind(this);
   }
 
-  componentDidMount() {
-    const docRef =  db.collection("cms/studyguide/toc");
- 
-     docRef.get().then((querySnapshot) => {
-       querySnapshot.forEach((doc) => {
+  async componentWillMount() {
+    // this.handleDropdown();
+    // console.log(this.state.questions)
+    const docRef =  await db.collection("cms/studyguide/toc");
+    
+    docRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         // console.log(doc);
-       this.state.questions.push(doc.id)
+        this.state.questions.push(doc.id)
       })
     })
-    // console.log(this.state.questions)
+    console.log('component will mount')
+  }
+  handleDropdown() {
+    
+
+    const item = document.getElementById('list-item');
+    const display = item.style.display;
+    item.style.display = display == "inherit" ? 'none' : "inherit"
   }
 
   render() {
+    console.log('render()')
     let { questions } = this.state;
 
     return(
@@ -50,17 +61,18 @@ export default class Navigation extends Component {
                 <Icon icon={home} size={18} className="icon"/>{' '}Home</Link>
             </li>
             <li>
-              <a href="#" className="bm-item-list">
+              <a href="#" className="bm-item-list" onClick={this.handleDropdown.bind(this)} id="toc">
                 <Icon icon={list2} size={18} className="icon" />{' '}
-                Table of Contents</a>
-              <ol>
+                Table of Contents
+              </a>
+              <ol id="list-item">
                 {questions.map((question) => 
                 <li><Link to="/page" className="bm-item-list indent" key={question}>{question}</Link></li>
                 )}               
               </ol>
             </li>
             <li><Link to="/editor" className="bm-item-list">
-              <Icon icon={user} size={18} className="icon" />{' '}Editor</Link>
+              <Icon icon={pencil} size={18} className="icon" />{' '}Editor</Link>
             </li>
             <li><Link to="/login" className="bm-item-list">
               <Icon icon={user} size={18} className="icon" />{' '}Login</Link>
